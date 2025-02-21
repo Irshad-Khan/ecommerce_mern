d9183c3086ce2c6c375fd30cbe86cd1353c359d1
muhammad.irshad <muhammad.irshad@pitb.gov.pk>
2025-02-21 20:31:49 +0500

    fixd

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const app = express();
 const ErrorMiddleware = require('./middlewares/ErrorsMiddleware');
 
 app.use(express.json());
+
+// Routes
 const products = require('./routes/product')
 const auth = require('./routes/auth')
 app.use('/api/v1', products);
